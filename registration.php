29a0b65d00f25bbdd96cc66f274c179c623fb84c
Arturo Cabrera <acabrera@ditelsa.com.gt>
2024-11-01 16:36:04 -0500

    [MOVED] this a module of testing to autologin in admin

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Actecnology Team
+ * @copyright Copyright (c) Actecnology (https://actecnology.com/)
+ * @package Actecnology_Autologin
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Actecnology_Autologin',
+    __DIR__
+);
